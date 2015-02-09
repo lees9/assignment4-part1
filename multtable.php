d8f361962ca6e57144d51f55b02f83ab577746c4
lees9 <lees9@onid.oregonstate.edu>
2015-02-09 12:30:34 -0800

    made variables and array

---
+++ multtable.php
@@ -0,0 +1,15 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Multtable.php</title>
+</head>
+<body>';
+
+$min_multiplicand;
+$max_multiplicand;
+$min_multiplier;
+$max_multiplier;
+
+$mult_array = array();
