6bc3ebd5abd7d002f6242892bfe639878d8c2bb8
lees9 <lees9@onid.oregonstate.edu>
2015-02-10 02:26:16 -0800

    loopback.php completed

--- loopback.php
+++ loopback.php
@@ -15,13 +15,30 @@ ini_set('display_errors', '1');
 </head>
 <body>
 <?php
+		//check method GET or POST
 		$method = $_SERVER['REQUEST_METHOD'];
-		$type = "TYPE";
-		$parameter = "Parameter";
-		$myArray = array(
-				$type => $method,
-				$parameter => array());
-		echo json_encode($myArray);
+
+		//if GET
+		if ($method == "GET"){
+			$myArray = array("Type" => $method, "parameters" => $_GET);
+			if (empty($_GET)){
+				foreach ($myArray as $key => $value){
+					$myArray['parameters'] = "null";	//change value to null if nothing entered
+				}
+			}
+		}
+
+		//if POST
+		if ($method == "POST"){
+			$myArray = array("Type" => $method, "parameters" => $_POST);
+			if (empty($_POST)){
+				foreach ($myArray as $key => $value){
+					$myArray['parameters'] = "null";	//change value to null if nothing entered
+				}
+			}
+		}
+		
+		echo json_encode($myArray, JSON_FORCE_OBJECT);
 ?>
 </body>
 </html>
